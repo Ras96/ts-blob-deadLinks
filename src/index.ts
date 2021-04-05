abd8e3f08841d796d7aa20d72f65852d2d5584f3
Ras96 <asymptote.k.k@gmail.com>
2021-04-05 18:18:59 +0900

    eslint prettier

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-import { findDeadLinks } from './func';
-import { postMessage, postFile } from './traqapi';
+import { findDeadLinks } from './func'
+import { postMessage, postFile } from './traqapi'
 
 const main = async () => {
-  postMessage(`Started checking deadlinks.(${process.env.WORK_ENV})`);
+  postMessage(`Started checking deadlinks.(${process.env.WORK_ENV})`)
   try {
-    const deadLinks = await findDeadLinks();
-    await postFile();
-    await postMessage(`Finished checking deadlinks. ${Object.keys(deadLinks).length} pages include deadlinks.`);
-    return;
+    const deadLinks = await findDeadLinks()
+    await postFile()
+    await postMessage(
+      `Finished checking deadlinks. ${
+        Object.keys(deadLinks).length
+      } pages include deadlinks.`
+    )
+    return
   } catch (err) {
-    await postMessage(`Error Found!\n\n${err}`);
-    throw err;
+    await postMessage(`Error Found!\n\n${err}`)
+    throw err
   }
-};
+}
 
 // 実行
-main();
+main()
